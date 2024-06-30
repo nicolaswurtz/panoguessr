1316747d6ae55badf668503fa74c84dd225b1722
Nicolas Wurtz <git@nethenic.net>
2024-06-30 09:25:48 +0200

    Cleaning

--- front/src/Home.tsx
+++ front/src/Home.tsx
@@ -1,24 +1,24 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
+import '@photo-sphere-viewer/core/index.css';
+import '@photo-sphere-viewer/virtual-tour-plugin/index.css';
 
 export default function Home() {
-  const ref = useRef();
+  const GeoVisioInit = async () => {
+    const GeoVisio = await import('geovisio');
+    const GeoVisioInstance = new GeoVisio.Viewer(
+      "viewer",  // Div ID
+      "https://api.panoramax.xyz/api",  // STAC API endpoint
+      { map: true }  // Viewer options
+    );
+  };
   
   useEffect(() => {
-    const GeoVisioInit = async () => {
-        const GeoVisio = await import("geovisio");
-        new GeoVisio.Viewer(
-          "viewer",  // Div ID
-          "https://api.panoramax.xyz/api",  // STAC API endpoint
-          { map: true }  // Viewer options
-        );
-    };
     GeoVisioInit();
-}, []);
+  }, []);
   
-  return ref && (
+  return (
     <>
       <div id="viewer" style={{width: '500px', height: '500px'}}></div>
-      Coucou
     </>
   );
 }
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import Home from './Home.tsx'
 import './main.scss'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Avoid StrictMode for now (it duplicates useEffect((),[])) in GeoVisio compenents
+/* ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>,
-)
+); */
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<Home />);
